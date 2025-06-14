a8c6a6789b74a04c268b961a2c6a18475e385dbc
Chung-Kelvin <chunglight01@gmail.com>
2025-06-15 02:32:19 +0700

    feat: init component header & add material icon

--- src/app/shared/component/component.module.ts
+++ src/app/shared/component/component.module.ts
@@ -1,25 +1,32 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 import {CarouselComponent} from './carousel/carousel.component';
 import {CardProductComponent} from './card-product/card-product.component';
 import {MatCardModule} from "@angular/material/card";
 import {MenuBarComponent} from './menu-bar/menu-bar.component';
+import {HeaderComponent} from './header/header.component';
 
 
 @NgModule({
   declarations: [
     CarouselComponent,
     CardProductComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    MatIconModule,
+    MatMenuModule
   ],
   exports: [
     CarouselComponent,
     CardProductComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    HeaderComponent
   ]
 })
 export class ComponentModule {
