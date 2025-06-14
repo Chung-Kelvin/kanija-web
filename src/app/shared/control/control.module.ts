53883be29a47519cd5e62a87130eff3bb1414b8b
Chung-Kelvin <chunglight01@gmail.com>
2025-06-15 03:03:17 +0700

    feat: init input component

---
+++ src/app/shared/control/control.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputComponent } from './input/input.component';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [InputComponent],
+  imports: [CommonModule],
+})
+export class ControlModule {}
