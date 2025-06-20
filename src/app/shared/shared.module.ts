839a05dd1a3a3227a5b48f03c1f4b1c52aee7cda
Chung-Kelvin <chunglight01@gmail.com>
2025-06-20 10:22:25 +0700

    feat: main content in layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,25 +1,31 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {LayoutComponent} from './layout/layout.component';
-import {RouterModule} from "@angular/router";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatIconModule} from "@angular/material/icon";
-import {ComponentModule} from "./component/component.module";
-import {LayoutAdminComponent} from "./layout-admin/layout-admin.component";
-import {NzBreadCrumbModule} from "ng-zorro-antd/breadcrumb";
-import {NzIconModule} from "ng-zorro-antd/icon";
-import {NzMenuModule} from "ng-zorro-antd/menu";
-import {NzLayoutModule} from "ng-zorro-antd/layout";
-
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { ComponentModule } from './component/component.module';
+import { LayoutAdminComponent } from './layout-admin/layout-admin.component';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 @NgModule({
-  declarations: [
-    LayoutComponent, LayoutAdminComponent
-  ],
+  declarations: [LayoutComponent, LayoutAdminComponent],
   imports: [
-    CommonModule, RouterModule, MatMenuModule, MatIconModule, ComponentModule, NzBreadCrumbModule, NzIconModule, NzMenuModule, NzLayoutModule, NzIconModule
+    CommonModule,
+    RouterModule,
+    MatMenuModule,
+    MatIconModule,
+    ComponentModule,
+    NzBreadCrumbModule,
+    NzIconModule,
+    NzMenuModule,
+    NzLayoutModule,
+    NzIconModule,
+    MatIconModule,
   ],
-  exports: [LayoutComponent, LayoutAdminComponent]
+  exports: [LayoutComponent, LayoutAdminComponent],
 })
-export class SharedModule {
-}
+export class SharedModule {}
